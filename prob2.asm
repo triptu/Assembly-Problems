f3509bb13c73d4d3a919002445045a0f9f6c247a
Tushar Tripathi <tushutripathi@gmail.com>
2017-03-01 18:25:46 +0530

    Prob 2

--- prob2.asm
+++ prob2.asm
@@ -1,50 +1,23 @@
-;Write an ALP that will examine a set of 26 memory locations that have alphabets and count the
-;number  of  vowels.  The  alphabets  are  store  from  memory  location
-;alph1 and  the  count  of  the vowels must be stored in location vcnt .
+;copy  a set of 10 bytes from memory location loc1 to memory location loc2 in the reverse order
 ; (c) Tushar Tripathi
 
 .model tiny
 .data
-dat1 db 'aAtuyseEghuUplmnIioOhjkluU'
-count db 26
-vcnt db 0
+loc1  db 'abcdefghij'
+loc2  db 10 DUP(?)
+count dw 10
 .code
 .startup
-    mov cl, count
+    mov cx, count
     mov bl, 0
-    lea si, dat1
-    check:
-        MOV al, BYTE PTR [si]
-        cmp al, 'a'
-        JE vowel
-        cmp BYTE PTR [si], 'A'
-        JE vowel
-        cmp BYTE PTR [si], 'e'
-        JE vowel
-        cmp BYTE PTR [si], 'E'
-        JE vowel
-        cmp BYTE PTR [si], 'i'
-        JE vowel
-        cmp BYTE PTR [si], 'I'
-        JE vowel
-        cmp BYTE PTR [si], 'o'
-        JE vowel
-        cmp BYTE PTR [si], 'O'
-        JE vowel
-        cmp BYTE PTR [si], 'u'
-        JE vowel
-        cmp BYTE PTR [si], 'U'
-        JE vowel
+    lea si, loc1
+    lea di, loc2
+    lea di, [di + 9]    ; alternative add di, 9
+    transfer:
+        mov al, [si]
+        mov BYTE PTR [di], al
         INC si
-        loop check
-    vowel:
-        INC bl
-        INC si
-        cmp cl, 0
-        je out2
-        loop check
-    out2:
-        NOP
-    mov [vcnt], bl
+        DEC di
+        loop transfer
 .exit
 end
